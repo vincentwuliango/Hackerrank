9fd863cdc6a9eb1a44f665885c042196e3468ea2
Vincent <122006254+vincentwuliango@users.noreply.github.com>
2023-02-17 20:21:35 +0700

    commit

---
+++ src/Solution1.java
@@ -0,0 +1,5 @@
+public class Solution1 {
+	public static void main(String[] args) {
+		System.out.printf("Hello, World.\nHello, Java.");
+	}
+}
---
+++ src/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		var i = 1;
+		var str = "";
+		while (i < 4) {
+			str += scan.nextLine();
+			if (i != 3) {
+				str += " ";
+			}
+			i++;
+		}
+		var result = str.split(" ");
+		
+		for (String a : result) {
+			System.out.println(Integer.valueOf(a));
+		}
+	}
+}
